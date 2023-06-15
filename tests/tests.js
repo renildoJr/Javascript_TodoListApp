7a294d990fda093d25f52a686c745cb7b32f327d
renildoJr <juninho952020@gmail.com>
2023-06-14 23:28:10 -0300

    crud test

--- tests/tests.js
+++ tests/tests.js
@@ -1,17 +1,19 @@
+const content = document.querySelector('.content')
+
 class Person {
     id;
     name;
     age;
     job;
-    constructor(name, age, job, id) {
+    constructor(name, bornYear, job, id) {
         this.id = id;
         this.name = name;
-        this.age = age;
+        this.age = new Date().getFullYear() - bornYear;
         this.job = job;
     }
 }
 
-function addPerson(name, age, job) {
+function addPerson(name, bornYear, job) {
     const findSameName = getPerson().find(obj=>obj.name.toLowerCase() === name.toLowerCase()) || false;
     
     if(findSameName) {
@@ -25,7 +27,7 @@ function addPerson(name, age, job) {
         lastId = previousData[previousData.length - 1].id + 1;
     }
 
-    const p = new Person(name, age, job, lastId);
+    const p = new Person(name, bornYear, job, lastId);
     previousData.push(p);
 
     return localStorage.setItem('person', JSON.stringify(previousData));
@@ -40,7 +42,6 @@ function getById(id) {
 }
 
 function removeById(id) {
-    
     const items = getPerson();
     const itemToRemove = items.findIndex(obj => obj.id === id)
     if(itemToRemove !== -1) {
@@ -50,12 +51,40 @@ function removeById(id) {
     return false;
 }
 
-function updateById(id, name, age, job) {
+function updateById(id, name, bornYear, job) {
     const items = getPerson();
     const itemToChange = items.findIndex((obj => obj.id === id ));
     items[itemToChange].name = name;
-    items[itemToChange].age = age;
+    items[itemToChange].age = bornYear;
     items[itemToChange].job = job;
     return localStorage.setItem('person', JSON.stringify(items))
-    
 }
+
+function listAll(sortBy) {
+    return getPerson().map(person=>
+        content.innerHTML+=`
+        <li class="card">
+            <input class="id" type="hidden" value="${person.id}">
+            <button class="btn" id="update">Edit</button>
+            <button class="btn" id="delete">Remove</button>
+            <div class="card-info">
+                <span class="info"><strong>Name: </strong>${person.name}</spa> 
+                <span class="info"><strong>Age: </strong>${person.age}</spa> 
+                <span class="info"><strong>Occupation: </strong>${person.job}</spa> 
+            </div>
+        </li>`
+    ) 
+}
+
+
+listAll('name')
+function crudOperations() {
+    const buttonList = document.querySelectorAll('.card .btn#delete');
+    document.querySelectorAll('.id').forEach(item=>ids.push(item.value));
+    buttonList.forEach((btn, index)=>btn.addEventListener('click', ()=>{
+        removeById(index)
+    }))
+}
+
+crudOperations()
+
